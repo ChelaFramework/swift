e1c20fd5c055dcd7108bfa9d89e37f8a1bad2c82
step4me <eyegochild@gmail.com>
2019-01-12 17:06:30 +0900

    holder 정의 중

--- ChelaS/view/router/holder/ChBase.swift
+++ ChelaS/view/router/holder/ChBase.swift
@@ -22,12 +22,3 @@ extension ChHolderBaseProtocol {
 class ChHolderBase<T>: ChHolderBaseProtocol {
 
 }
-
-
-protocol ChHolderProtocol {
-  associatedtype T
-}
-class ChHolder<T>: ChHolderProtocol {
-
-}
-
--- ChelaS/view/router/holder/ChHolder.swift
+++ ChelaS/view/router/holder/ChHolder.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import UIKit
+
+//abstract class ChHolder<T>{
+//  @JvmField internal var key:String = ""
+//  init{
+//    @Suppress("LeakingThis")
+//    this::class.simpleName?.let{key = it}
+//  }
+//  abstract fun create(base: ChHolderBase<T>):T
+//  open fun push(base: ChHolderBase<T>, isRestore:Boolean){}
+//  open fun resume(base: ChHolderBase<T>, isRestore:Boolean){}
+//  open fun pause(base: ChHolderBase<T>, isJump:Boolean){}
+//  open fun pop(base: ChHolderBase<T>, isJump:Boolean):Boolean = true
+//}
+protocol ChHolderProtocol {
+  associatedtype T
+  func create<T>(base: ChHolderBase<T>) -> T
+  func push(base: ChHolderBase<T>, isRestore: Bool)
+  func resume(base: ChHolderBase<T>, isRestore: Bool)
+  func pause(base: ChHolderBase<T>, isJump: Bool)
+  func pop(base: ChHolderBase<T>, isJump: Bool)
+}
+class ChHolder<T: UIView>: ChHolderProtocol {
+  func create<T>(base: ChHolderBase<T>) -> T {
+    return UIView.init()
+  }
+}
+
+//object Main : ChHolder<View>() {
+//  override fun create(base: ChHolderBase<View>): View {
+//    if(base !is ChGroupBase) throw Exception("invalid holder_alpha:$base")
+//    //Splash.model.start()
+//    return Ch.scanner.scan(this, base.inflate(R.layout.main)).render()
+//  }
+//
+//}
+
+extension ChHolderProtocol where T == UIView {
+  func create<T>(base: ChHolderBase<T>) -> T {
+    return UIView.init() as! T
+  }
+}
+
+extension ChHolderProtocol {
+  func push(base: ChHolderBase<T>, isRestore: Bool) {
+    
+  }
+  
+  func resume(base: ChHolderBase<T>, isRestore: Bool) {
+    
+  }
+  
+  func pause(base: ChHolderBase<T>, isJump: Bool) {
+    
+  }
+  
+  func pop(base: ChHolderBase<T>, isJump: Bool) {
+    
+  }
+}
+
+
