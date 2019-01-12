26f77b9ee7fb403897eb7f91ed6657c0de639691
dotNetTree <ksc214@gmail.com>
2019-01-12 19:12:31 +0900

    router 구현 중

--- ChelaS/view/router/ChRouter.swift
+++ ChelaS/view/router/ChRouter.swift
@@ -19,12 +19,50 @@ class ChRouter<T, H: ChHolder> where H.T == T {
     // TODO: self.base.router = self
   }
   func restore() {
-//    stack.forEach { it in
-////      base._push(holder: it)
-//    }
+    stack.forEach { it in
+//      base._push(holder: it)
+    }
   }
-  func push(holder: H) {
+
+  func unlockPush() {
+    if pushLock { pushLock = false }
+  }
+  func unlockPop() {
+    if popLock { popLock = false }
+  }
+  func push(holder: H, _ isAutoUnlock: Bool = true) {
+    guard pushLock else { return }
+    if !isAutoUnlock { pushLock = true }
+    if let last = stack.last {
+//      base._pause(last, false)
+    }
+//    base._push(holder, false)
     stack.append(holder)
+  }
+  func pop(_ isAutoUnlock: Bool = true) -> Int {
+    guard !popLock else { return -1 }
+    if !isAutoUnlock { popLock = true }
+
+    if let last = stack.last {
+//      base._pop(last, false)
+      stack.removeLast()
+      if let last = stack.last {
+//        base._resume(last, false)
+      }
+      return stack.count
+    } else {
+        return 0
+    }
+  }
+  func jump(holder: H) {
+    for i in (stack.count - 1)...0 {
+      if stack[i] === holder {
+//        base._resume(holder, false)
+        break
+      } else {
+//        base._pop(holder, true)
+      }
+    }
   }
 }
 
--- ChelaS/view/router/holder/ChHolder.swift
+++ ChelaS/view/router/holder/ChHolder.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-protocol ChHolder {
+protocol ChHolder: class {
   associatedtype T
   func create(base: ChHolderBase<T>) -> T
 }
