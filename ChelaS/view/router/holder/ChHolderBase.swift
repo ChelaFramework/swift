bca595a06174012ec0eb7b5d17e086bd57c57cee
step4me <eyegochild@gmail.com>
2019-01-12 19:15:09 +0900

    ChHolderbase 정의중

--- ChelaS/view/router/holder/ChHolderBase.swift
+++ ChelaS/view/router/holder/ChHolderBase.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
+//abstract class ChHolderBase<T>{
+//  internal lateinit var router: ChRouter<T>
+//  internal fun _push(holder: ChHolder<T>, isRestore:Boolean){
+//  push(holder)
+//  holder.push(this, isRestore)
+//  _resume(holder, isRestore)
+//  }
+//  internal fun _pop(holder: ChHolder<T>, isJump:Boolean){
+//  _pause(holder, isJump)
+//  if(holder.pop(this, isJump)) pop(holder)
+//  }
+//  internal fun _pause(holder: ChHolder<T>, isJump:Boolean){
+//  pause(holder)
+//  holder.pause(this, isJump)
+//  }
+//  internal fun _resume(holder: ChHolder<T>, isRestore:Boolean){
+//  resume(holder)
+//  holder.resume(this, isRestore)
+//  }
+//  protected fun restore(){router.restore()}
+//  protected open fun push(holder: ChHolder<T>){}
+//  open fun pop(holder: ChHolder<T>){}
+//  protected open fun pause(holder: ChHolder<T>){}
+//  protected open fun resume(holder: ChHolder<T>){}
+//}
+
 protocol ChHolderBaseProtocol {
   associatedtype T
 //  func _push(holder: ChHolder<T>)
@@ -19,6 +45,13 @@ extension ChHolderBaseProtocol {
 //  }
 }
 
-class ChHolderBase<T>: ChHolderBaseProtocol {
+class ChHolderBase<T, H: ChHolder> where H.T == T {
 
+  init() { }
+  internal func _push(holder: H, isRestore:Bool) {
+    
+  }
+  
+  fileprivate func push() {}
 }
+
