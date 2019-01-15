3234c411c519379b8759d79cff8c6af2a832f843
dotNetTree <ksc214@gmail.com>
2019-01-15 21:13:09 +0900

    net 작업

---
+++ ChelaS/net/ChHttp.swift
@@ -0,0 +1,13 @@
+//
+//  ChHttp.swift
+//  Chela
+//
+//  Created by SeungChul Kang on 2019. 1. 15..
+//  Copyright © 2019년 Chela. All rights reserved.
+//
+
+import Foundation
+
+class ChHttp {
+  var extra = [String: Any]()
+}
