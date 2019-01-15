a20a983bb2ed80f0a65c7ddac2677231ee2bcdae
step4me <eyegochild@gmail.com>
2019-01-15 23:16:26 +0900

    ChLooper 작업중

--- ChelaS/looper/ChLooper.swift
+++ ChelaS/looper/ChLooper.swift
@@ -13,5 +13,35 @@ typealias Now = ()->Double
 internal let empty: ItemBlock = {_ in }
 internal let now: Now = { Date().timeIntervalSinceNow }
 class ChLooper {
+  enum Item {
+    case Time(ms: Int)
+    case Delay(ms: Int)
+    case Loop(cnt: Int)
+    case Ended(block: (ChItem) -> Void)
+    case Infinity
+  }
+
+// https://github.com/ChelaFramework/ios/blob/9102f458a50695d04e816313c4f8094febe5c1c5/Empty/MV/ChLooper.m
+//  private class Ani: UIView() {
+//    init() {
+//
+//    }
+//  }
+  
+//  private class Ani(ctx: Context, private val looper: ChLooper): View(ctx){
+//  init{tag = "CHELA_ANI"}
+//  override fun onDraw(canvas: Canvas?){
+//  looper.loop()
+//  invalidate()
+//  }
+//  }
+  
+  class Sequence {
+    private let looper: ChLooper
+//    internal var item: ChItem
+    init(looper: ChLooper) {
+      self.looper = looper
+    }
+  }
   
 }
