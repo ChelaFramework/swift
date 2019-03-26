ca7fafa95a13861cd86f4b6defbef44b7acf1c55
Taehyeon Jake Lee <nayalth@gmail.com>
2019-03-26 23:10:36 +0900

    ✨ CocoaPods 없이 Alamofire 적용

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
     viewBase.view(view)
     router.push(holder: Splash.shared, true)
-//    let net = ChNet.init()
-//    let http = net.http(url: "https://www.naver.com")
-//    http?.send { res in
-//      guard let data = res.byte else {
-//        return
-//      }
-//      print("res: \(data)")
-//    }
+    let net = ChNet.init()
+    let http = net.http(url: "https://www.naver.com")
+    http?.send { res in
+      guard let data = res.byte else {
+        return
+      }
+      print("res: \(data)")
+    }
 
 //    ChHttp.init(method: "GET", url: "https://www.google.com/search?q=strongpassword+receiver+ios&newwindow=1&rlz=1C5CHFA_enKR826KR827&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjo4bOGgJPgAhVFUN4KHU3mC00Q_AUIDigB&biw=1680&bih=940#imgrc=RzRo1ST8AgwIEM:")?
 //    .send(callback: { res in
